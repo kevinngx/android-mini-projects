9f2e021b3f9aa3d5c3a80cdc681a031d934c245c
Kevin Nguyen <kevinngx625@gmail.com>
2024-07-26 06:21:49 +1000

    Sleep Quality Tracker

---
+++ SleepQualityTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.app.Application
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.AndroidViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+
+class SleepTrackerViewModel(
+        val database: SleepDatabaseDao,
+        application: Application) : AndroidViewModel(application) {
+
+
+}
