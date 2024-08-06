faaea3c24367ffb889a1dc214e1d24a06d66153b
Kevin Nguyen <kevinngx625@gmail.com>
2024-08-06 12:46:17 +1000

    Canvas Setup

---
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android.minipaint
+
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val myCanvasView = MyCanvasView(this)
+//        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
